b9b81ffb687e06fdfc1e0414b745e0edd9f32f1e
HP <HP@DESKTOP-HC76K2O>
2021-12-06 13:54:52 +0530

    new path ADD

--- Nov01_2021/src/test/java/pages/NEWPAGE.java
+++ Nov01_2021/src/test/java/pages/NEWPAGE.java
@@ -1,5 +1,13 @@
 package pages;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NEWPAGE {
+	void opnbrewser()
+	{
+		System.setProperty("", "");
+		ChromeDriver d=new ChromeDriver();
+		
+	}
 
 }
